584e968448260feb0a46d3b935cd36053ddf3fd4
0v-0 <73421739+0v-0@users.noreply.github.com>
2024-08-26 19:37:11 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+  readyStatus: false,
+  checkUpdate: false
+});
+//environment
+require('dotenv').config()
+
+function formatTime() { 
+  const date = new Date();
+  const options = {
+    timeZone: 'America/New_York', 
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric'
+  };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+}
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>)
+});
+client.on('ready', async () => {
+  console.clear();
+  console.log(${client.user.tag} - rich presence started!);
+
+  const r = new Discord.RichPresence()
+    .setApplicationId('1265825059692609587')
+    .setType('PLAYING')
+    .setURL('https://www.twitch.tv/apparentlyjack_rl') 
+    .setState('Hey Nitro is here')
+    .setName('quaaxz')
+    .setDetails(Nitro is now)
+    .setStartTimestamp(Date.now())
+ .setAssetsLargeImage('image_2.webp') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Nitro') 
+    .setAssetsSmallImage('checked.png') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Small Text') 
+    .addButton('Google', 'https://google.com'/);
+
+  client.user.setActivity(r);
+  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+
+});
+
+const mySecret = process.env['TOKEN'];
+client.login(mySecret);
